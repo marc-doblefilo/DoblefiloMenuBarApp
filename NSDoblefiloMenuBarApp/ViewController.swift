becd5dbce1e7070cc432bad42b5b87b1f1ac6366
Marc Rodriguez <rodriguezmoreno.marcos@gmail.com>
2022-10-05 10:12:03 +0200

    Initial Commit

---
+++ NSDoblefiloMenuBarApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NSDoblefiloMenuBarApp
+//
+//  Created by Marc Rodríguez on 5/10/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
